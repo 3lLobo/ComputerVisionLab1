3cee059fc70621822b0dec5e58f92f734d4a5b06
alex-lindt <alex.lindt@protonmail.com>
2019-02-21 20:32:12 +0100

    gaussian kernels

--- Assignment_2/Part-1: Image Enhancement/gauss1D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss1D.m
@@ -1,7 +1,14 @@
 function G = gauss1D( sigma , kernel_size )
-    G = zeros(1, kernel_size);
+
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
-    %% solution
+    
+    % create x
+    fks = floor(kernel_size/2);
+    x = -fks:1:fks;
+    % compute gaussian filter for x
+    G = normpdf(x, 0, sigma);
+    % normalize filter
+    G = G / sum(G(:));       
 end
--- Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss2D.m
@@ -1,3 +1,7 @@
 function G = gauss2D( sigma , kernel_size )
-    %% solution
+    G_x = gauss1D(sigma, kernel_size);
+    % Note: G_x and G_y in the given formel are the same, 
+    % only transposed. Therefore we only need to compute one
+    % gauss1D here.
+    G = (G_x)'*G_x;
 end
