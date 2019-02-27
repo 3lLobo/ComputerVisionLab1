44ba2d494189021f816e64dc7fac4e0fdd003c6e
David Biertimpel <david.biertimpel@protonmail.com>
2019-02-27 21:30:52 +0100

    Bugfix create Gabor

--- Assignment_2/Part-2: Gabor Segmentation/createGabor.m
+++ Assignment_2/Part-2: Gabor Segmentation/createGabor.m
@@ -66,9 +66,9 @@ myGabor_imaginary = gaussianEnv.*sinCarrier;
 myGabor(:,:,1) = myGabor_real;
 myGabor(:,:,2) = myGabor_imaginary;
 
-figure;
-subplot(121), imshow(myGabor_real,[]);
-subplot(122), imshow(myGabor_imaginary, []);
+% figure;
+% subplot(121), imshow(myGabor_real,[]);
+% subplot(122), imshow(myGabor_imaginary, []);
 end
 
 
