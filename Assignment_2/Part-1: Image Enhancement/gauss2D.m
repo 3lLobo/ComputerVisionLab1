00f0eb090de826159d83ce0c258a45558759c7a2
alex-lindt <alex.lindt@protonmail.com>
2019-03-07 10:43:30 +0100

    remove doubled folders

--- Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++
@@ -1,7 +0,0 @@
-function G = gauss2D( sigma , kernel_size )
-    G_x = gauss1D(sigma, kernel_size);
-    % Note: G_x and G_y in the given formel are the same, 
-    % only transposed. Therefore we only need to compute one
-    % gauss1D here.
-    G = (G_x)'*G_x;
-end
