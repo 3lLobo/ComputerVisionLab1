1908f8eef21b9dbeda26c43d6fe5070a2e9b7b18
alex-lindt <43879154+alex-lindt@users.noreply.github.com>
2019-03-13 19:40:32 +0100

    update demo

--- Assignment_4/stitch_demo.m
+++ Assignment_4/stitch_demo.m
@@ -1,16 +1,11 @@
+%run vlfeat/toolbox/vl_setup
+
 im_left = imread('left.jpg');
 im_right = imread('right.jpg');
-
-
 N = 50;
 K = 10;
 
-%run vlfeat/toolbox/vl_setup
-
+% run stitch.m for test images
 img = stitch(im_left, im_right, N, K);
-
 figure;
 imshow(img);
-
-%figure;
-%imshow(stitched_image);
