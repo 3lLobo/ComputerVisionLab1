f86e2b8a5acac884fc92152e91b9217d11c25357
3lLobo <flocwolf@gmail.com>
2019-03-18 19:59:13 +0100

    Project

---
+++ Project/make_opponent.m
@@ -0,0 +1,16 @@
+function [oppo] = make_opponent(image)
+
+width = size(image,1);
+
+r = image(:,:,1);
+g = image(:,:,2);
+b = image(:,:,3);
+
+oppo = zeros(width,width,3);
+oppo(:,:,1) = (r-g)/sqrt(2);
+oppo(:,:,2) = (r+g-2*b)/sqrt(6);
+oppo(:,:,3) = (r+g+b)/sqrt(3);
+
+oppo = uint8(oppo);
+
+end
