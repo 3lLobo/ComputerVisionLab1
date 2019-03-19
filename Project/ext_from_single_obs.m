bb50b4f34a6bc1772f525485fd4def421ad9ba11
lucas <lucas.fijen@gmail.com>
2019-03-19 18:55:59 +0100

    Prepared obs for sifttype

--- Project/ext_from_single_obs.m
+++ Project/ext_from_single_obs.m
@@ -1,4 +1,4 @@
-function descriptors = ext_from_single_obs(obervation, type)
+function descriptors = ext_from_single_obs(obervation, type, sift_type)
 
     im_rgb = reshape(obervation, 96, 96, 3);
     im_gs = rgb2gray(im_rgb);
@@ -7,34 +7,41 @@ function descriptors = ext_from_single_obs(obervation, type)
 
     if type=="rgb"
         I_rgb = im2single(im_rgb);
-        
-        descriptors = [];
-        for channel_i = 1:size(I_rgb, 3)
-            channel = I_rgb(:, :, channel_i);
-            [~, d_channel] =  vl_sift(channel, 'Frames', f_gs);
-            descriptors = [descriptors ; d_channel];
-        end
-        descriptors = transpose(descriptors);  
+        descriptors = helperfunction(I_rgb, f_gs, sift_type);  
 
     elseif type=="opponent"
         im_opp = make_opponent(im_rgb);
         I_opp = im2single(im_opp);
-        
-        descriptors = [];
-        for channel_i = 1:size(I_opp, 3)
-            channel = I_opp(:, :, channel_i);
-            [~, d_channel] =  vl_sift(channel, 'Frames', f_gs);
-            descriptors = [descriptors ; d_channel];
-        end
-        descriptors = transpose(descriptors);
+        descriptors = helperfunction(I_opp, f_gs, sift_type);
     
     elseif type=="gray"
-        descriptors = transpose(d_gs);
+        descriptors = d_gs;
 
     else
         disp("Wrong color type as input?")
         return
     
     end
+    
+    descriptors = transpose(descriptors);
 
+end
+
+function descriptors = helperfunction(I, f_gs, sift_type)
+    if sift_type == "dense"
+        binSize = 8 ;
+        magnif = 3 ;
+        I = vl_imsmooth(I, sqrt((binSize/magnif)^2 - .25));
+    end
+    
+    descriptors = [];
+    for channel_i = 1:size(I, 3)
+        channel = I(:, :, channel_i);
+        if sift_type == "dense"
+            [~, d_channel] = vl_dsift(channel, 'size', binSize);
+        else
+            [~, d_channel] =  vl_sift(channel, 'Frames', f_gs);
+        end
+        descriptors = [descriptors ; d_channel];
+    end
 end
